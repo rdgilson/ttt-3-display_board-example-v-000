85df1daae6bd1164d6fbd0637f95dcc21c579ed6
Randy Gilson <randygilson@gmail.com>
2017-10-15 02:49:19 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,13 +1,13 @@
 def display_board
   board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-  puts " #{board[0]} |  #{board[1]}| #{board[2]}  "
-  " #{board[0]} |  #{board[1]}| #{board[2]}  "
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   "-----------"
-  puts " #{board[3]} |  #{board[4]}| #{board[5]}  "
-  " #{board[3]} |  #{board[4]}| #{board[5]}  "
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   "-----------"
-  puts " #{board[6]} |  #{board[7]}| #{board[8]}  "
-  " #{board[6]} |  #{board[7]}| #{board[8]}  "
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  " #{board[6]} | #{board[7]} | #{board[8]} "
 end
